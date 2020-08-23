0e57bc3df0b78594146cb926471eaa1c663f2db1
Chenemi Zekeri <chenemi2@gmail.com>
2020-08-23 23:02:56 +0100

    PWA functionality added

--- script.js
+++ script.js
@@ -176,4 +176,14 @@ function deleteBudget() {
     displayBudgetContent.classList.add('hide');
 }
 
+//register service worker
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+        navigator.serviceWorker
+            .register("/serviceWorker.js")
+            .then(res => console.log("service worker registered"))
+            .catch(err => console.log("service worker not registered", err))
+    })
+}
+
 //implement function to delete individual categories
